0a79e19c4ba4c36d6c23bdeb07cc373e23c34d6e
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro.local>
2019-05-03 16:04:17 -0600

    update v1.2

--- server/index.js
+++ server/index.js
@@ -1,15 +1,14 @@
 const express = require('express');
-
 const products = require('../products.json')
 
-const app = express()
+const app = express();
 const port = 3002;
 
 app.get('/api/products', (req, res) => {
     return res.status(200).send(products);
 })
 
-app.listen(port, () => (
-    console.log(`Listening on port: ${port}`)
-))
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`)
+});
 
